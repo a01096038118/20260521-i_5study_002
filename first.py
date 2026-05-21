be65777f969e421be301e33d69b241f1a7deafe2
riyoon <a0196038118@gmail.com>
2026-05-21 11:04:13 +0900

    20260521 by study_003

--- first.py
+++ first.py
@@ -15,7 +15,7 @@ members = {}
 
 
 while flag:
-    selectedMenuNum = int(input('1.회원가입  2.로그인   3.나의 회원 정보 출력  4.모든 회원 정보 출력  5. 회원 탈퇴 6. 회원정보 수정 99.종료'))
+    selectedMenuNum = int(input('1.회원가입  2.로그인   3.나의 회원 정보 출력  4.모든 회원 정보 출력  5. 회원 탈퇴 6. 회원정보 수정 7. ID/PW 찾기 99.종료'))
     if selectedMenuNum == 1:
         
         uID = input('회원ID:  ')
@@ -81,24 +81,64 @@ while flag:
 
     if selectedMenuNum == 6:
         uID = input('회원ID: ')
-        while True:
-            uPW = input('회원PW: ')
-            
-            if uID in members:
-                uInfo = members[uID]
-                if uInfo ['uPW'] == uPW:
+        uPW = input('회원PW: ')
+        if uID in members:
+            uInfo = members[uID]
+            if uInfo ['uPW'] == uPW:
+                print('인증 성공')
 
-            # else: 
-            #     print('비밀번호를 다시 입력해주세요.')
-            #     for key, value in uInfo.items():
-            #         print(f'{key}: {value}')  
+                updateKey = input('수정할 목록 입력: ')
+                updateValue = input('새로운 값 입력: ')
 
-            #         updateKey = input('수정할 목록 입력: ')
-            #         updateValue = input('새로운 값 입력: ')
+                uInfo[updateKey] = updateValue
+                for key, value in uInfo.items():
+                    print(f'{key}: {value}') 
+                    
+    if selectedMenuNum == 7:
+        selectedFindMenu = int(input('ID만 찾으실 분은 1번 , PW만 찾으실 분은: 2번 , 둘다 찾으실 분은 3번을 입력해주세요. '))
+        if selectedFindMenu == 1:
+            print('아이디 찾기를 하시려면 회원님의 이메일과 전화번호를 입력해주세요.')
+            uEmail = input('회원 EMAIL: ')
+            uPhone = input('회원 PHONE: ')
+            found = False
 
-            #         uInfo[updateKey] = updateValue
-            #         for key, value in uInfo.items():
-            #             print(f'{key}: {value}') 
+            for key, value in members.items():
+                if value['uEMAIL'] == uEmail and value['uPHONE'] == uPhone:
+                    print(f'회원님의 아이디: {key} ')
+                    found = True
+                    break
+
+                if found == False:
+                    print('찾을 수 없는 회원 정보가 없습니다.')
+
+        if selectedFindMenu == 2:
+            print('비밀번호 찾기를 하시려면 회원님의 이메일과 전화번호를 입력해주세요.')
+            uEmail = input('회원 EMAIL: ')
+            uPhone = input('회원 PHONE: ')
+            found = False
+            for key, value in members.items():
+                if value['uEMAIL'] == uEmail and value['uPHONE'] == uPhone:
+                    print(f'회원님의 비밀번호: {value['uPW']} ')
+                    found = True
+                    break
+
+            if found == False:
+                print('찾을 수 없는 회원 정보가 없습니다.')            
+
+
+        if selectedFindMenu == 3:
+            print('아이디/비밀번호 찾기를 하시려면 회원님의 이메일과 전화번호를 입력해주세요.')
+            uEmail = input('회원 EMAIL: ')
+            uPhone = input('회원 PHONE: ')
+            found = False
+            for key, value in members.items():
+                if value['uEMAIL'] == uEmail and value['uPHONE'] == uPhone:
+                    print(f'회원님의 아이디: {key}, 회원님의 비밀번호: {value['uPW']} ')
+                    found = True
+                    break
+
+            if found == False:
+                print('찾을 수 없는 회원 정보가 없습니다.')
 
     if selectedMenuNum == 99:
         print('프로그램을 종료합니다. ')
